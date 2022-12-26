bb1fb46fc7fdd0424b45bba99802e10af8cc167c
Thành Long <thanhlong11a11@gmail.com>
2022-12-26 16:10:44 +0700

    Làm UI phần Heder 4

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')} 
+                src=""
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
